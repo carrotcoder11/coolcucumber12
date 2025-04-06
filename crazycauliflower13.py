e9884a4303f80bf40e07271b8231e56018234c88
carrotcoder11 <samsonsky8@gmail.com>
2025-04-06 14:56:08 +0000

    begin tic tac toe

--- crazycauliflower13.py
+++ crazycauliflower13.py
@@ -1,1 +1,12 @@
-print("brainlessbroccoli14")
+brainlessbroccoli14 = [["_","_","_"],["_","_","_"],["_","_","_"]]
+def printMatrix(m):
+    for i in m:
+        print(i)
+trashytomato15=False
+def amazingasparagus16(m):
+    return False
+while not trashytomato15:
+    printMatrix(brainlessbroccoli14)
+    politepepper17 = input("Where would you like to move? ")
+    #update matrix
+    trashytomato15 = amazingasparagus16(brainlessbroccoli14)
